01f7aa295a1b58c6127a872100b65439f2e047a6
Mayank Gagrani <m.gagrani01@gmail.com>
2024-04-25 01:31:32 +0530

    tdd add numbers

--- app/services/tdd_calculator.rb
+++ app/services/tdd_calculator.rb
@@ -1,6 +1,8 @@
 class TddCalculator
   def self.add(numbers_string)
     return 0 if numbers_string.empty?
+    numbers = numbers_string.split(',').map(&:to_i) # sum of number in string
+    numbers.sum
   end
 end
 
--- spec/services/tdd_calculator_spec.rb
+++ spec/services/tdd_calculator_spec.rb
@@ -8,5 +8,25 @@ RSpec.describe TddCalculator do
        puts "returns 0 for an empty string"
      end
 
+     it 'returns sum for an string' do
+       result = TddCalculator.add("1")
+       expect(result).to eq(1)
+       puts "returns 1 for an empty string"
+     end
+
+     it 'returns sum for an string' do
+       result = TddCalculator.add("0,5")
+       expect(result).to eq(5)
+     end
+
+     it 'returns 9 for an string' do
+       result = TddCalculator.add("2,1,6")
+       expect(result).to eq(9)
+     end
+
+     it 'returns 13 for an empty string' do
+       result = TddCalculator.add("1,5,2,5")
+       expect(result).to eq(13)
+     end
    end
 end
